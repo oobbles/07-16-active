fdfa6f48a0ee64c506ee999254437e6627fb3251
Lisa Davis <oobbles@gmail.com>
2015-07-18 19:32:28 -0500

    changed database

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require "sinatra/reloader"
 
 configure :development do
   require "sqlite3"
- ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'photo_albums.db')
+ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'photography.db')
  end
 
  configure :production do
